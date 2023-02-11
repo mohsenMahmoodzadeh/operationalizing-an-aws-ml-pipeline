ffac1b4bf62e0b96e4846562eac657fc07472b69
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2023-02-11 22:52:42 +0330

    [add] endpoint name

--- lamdafunction.py
+++ lamdafunction.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 print('Loading Lambda function')
 
 runtime=boto3.Session().client('sagemaker-runtime')
-endpoint_Name='BradTestEndpoint'
+endpoint_name='dog-pytorch-endpoint-multi-instance'
 
 def lambda_handler(event, context):
 
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
     bs=event
     runtime=boto3.Session().client('sagemaker-runtime')
     
-    response=runtime.invoke_endpoint(EndpointName=endpoint_Name,
+    response=runtime.invoke_endpoint(EndpointName=endpoint_name,
                                     ContentType="application/json",
                                     Accept='application/json',
                                     #Body=bytearray(x)
